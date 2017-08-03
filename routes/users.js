cf78880aef0986e0bb524d388df213d1ebdfd83a
mrmjcf <mrmjcf@gmail.com>
2017-08-03 16:26:21 +0000

    Check new changelog

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Quiz = require('../models/quiz');
 var xssFilters = require('xss-filters');
+var stringSimilarity = require('string-similarity')
+
+//stringSimilarity.compareTwoStrings()
 
 // xssFilters.inHTMLData(firstname)
 
 router.get('/a/about', function(req, res, next) {
     res.render('about');
+})
+
+router.get('/a/changelog', function(req, res, next) {
+    res.render('changelog');
 })
 
 router.get('/quizmaker', function(req, res, next) {
@@ -20,20 +27,20 @@ router.get('/', function(req, res, next) {
 
 //Redirect username to...
 router.post('/quizmaker', function(req, res, next) {
-    var hour = 3600000;
     res.redirect('/quizmaker/' + req.body.username);
 });
 
 //...let the user fill out a form that will eventually...
-router.get('/quizmaker/:username', function(req, res, next){
+router.get('/quizmaker/:username', function(req, res, next) {
     var paramUsername = xssFilters.inHTMLData(req.params.username);
-    Quiz.count({ "username": paramUsername, "isKey": true}, function(err, numOfKeys) {
-        if(err) { 
+    Quiz.count({ "username": paramUsername, "isKey": true }, function(err, numOfKeys) {
+        if (err) {
             res.send('Illegal Request', 400);
         }
         if (numOfKeys == 0) {
             res.render('quizmaker', { username: paramUsername, error: '' });
-        } else {
+        }
+        else {
             res.redirect('/entries/' + paramUsername);
         }
     });
@@ -50,20 +57,20 @@ function isEmpty(variable) {
 
 function comparingResults(answerKey, newAttempt) {
     var score = 100;
-    if (newAttempt.color != answerKey.color) { score -= 11 }
-    if (newAttempt.currentage != answerKey.currentage) { score -= 11 }
-    if (newAttempt.crush != answerKey.crush) { score -= 11 }
-    if (newAttempt.movie != answerKey.movie) { score -= 11 }
-    if (newAttempt.pet != answerKey.pet) { score -= 11 }
-    if (newAttempt.snack != answerKey.snack) { score -= 11 }
-    if (newAttempt.enemy != answerKey.enemy) { score -= 11 }
-    if (newAttempt.favsong != answerKey.favsong) { score -= 11 }
-    if (newAttempt.sadsong != answerKey.sadsong) { score -= 11 }
+    if (stringSimilarity.compareTwoStrings(newAttempt.color.trim().toLowerCase(), answerKey.color.trim().toLowerCase()) < .8) { score -= 11 }
+    if (newAttempt.currentage != answerKey.currentage) { score -= 11 } //NUMBER
+    if (stringSimilarity.compareTwoStrings(newAttempt.crush.trim().toLowerCase(), answerKey.crush.trim().toLowerCase()) < .8) { score -= 11 }
+    if (stringSimilarity.compareTwoStrings(newAttempt.movie.trim().toLowerCase(), answerKey.movie.trim().toLowerCase()) < .8) { score -= 11 }
+    if (newAttempt.pet != answerKey.pet) { score -= 11 } //NUMBER
+    if (stringSimilarity.compareTwoStrings(newAttempt.snack.trim().toLowerCase(), answerKey.snack.trim().toLowerCase()) < .8) { score -= 11 }
+    if (stringSimilarity.compareTwoStrings(newAttempt.enemy.trim().toLowerCase(), answerKey.enemy.trim().toLowerCase()) < .8) { score -= 11 }
+    if (stringSimilarity.compareTwoStrings(newAttempt.favsong.trim().toLowerCase(), answerKey.favsong.trim().toLowerCase()) < .8) { score -= 11 }
+    if (stringSimilarity.compareTwoStrings(newAttempt.sadsong.trim().toLowerCase(), answerKey.sadsong.trim().toLowerCase()) < .8) { score -= 11 }
     return score;
 }
 
 //...check to see errors (and might redirect) but eventually...
-router.post('/quizmaker/:username', function(req, res, next){
+router.post('/quizmaker/:username', function(req, res, next) {
     var paramUsername = xssFilters.inHTMLData(req.params.username);
     var pendingQuizAnswer =
         new Quiz({
@@ -89,7 +96,8 @@ router.post('/quizmaker/:username', function(req, res, next){
         isEmpty(pendingQuizAnswer.snack) ||
         isEmpty(pendingQuizAnswer.enemy) ||
         isEmpty(pendingQuizAnswer.favsong) ||
-        isEmpty(pendingQuizAnswer.sadgong)) {
+        isEmpty(pendingQuizAnswer.message) ||
+        isEmpty(pendingQuizAnswer.sadsong)) {
         res.render('quizmaker', {
             "username": paramUsername,
             "error": 'Hey! You might want to fill out all inputs!'
@@ -113,19 +121,19 @@ router.post('/quizmaker/:username', function(req, res, next){
 //...let them go to their 'dashboard' and see other people's entries.
 router.get('/entries/:username', function(req, res, next) {
     var paramUsername = xssFilters.inHTMLData(req.params.username);
-    Quiz.find({ "username": paramUsername, "isKey": true }, function(err, setting){
+    Quiz.find({ "username": paramUsername, "isKey": true }, function(err, setting) {
         if (err) {
             res.send('Illegal Request. Reported', 400);
         }
-        if(setting.length == 1) {
-            Quiz.find({"username": paramUsername, "isKey": false}, function(err, entries){
+        if (setting.length >= 1) {
+            Quiz.find({ "username": paramUsername, "isKey": false }, function(err, entries) {
                 if (err) {
                     res.send('Illegal request. Reported.', 400);
                     console.log(err);
                 }
-                if(entries.length > 1) {
-                    var scores = []; 
-                    for(var i = 0;i < entries.length; i++) {
+                if (entries.length > 0) {
+                    var scores = [];
+                    for (var i = 0; i < entries.length; i++) {
                         scores.push(comparingResults(setting[0], entries[i]));
                     }
                     res.render('quizentries', {
@@ -134,7 +142,8 @@ router.get('/entries/:username', function(req, res, next) {
                         "currentSetting": setting,
                         "scores": scores,
                     });
-                } else {
+                }
+                else {
                     res.render('quizentries', {
                         "username": paramUsername,
                         "entries": "[]",
@@ -143,7 +152,8 @@ router.get('/entries/:username', function(req, res, next) {
                     });
                 }
             });
-        } else {
+        }
+        else {
             res.redirect('/quizmaker/' + paramUsername);
         }
     });
@@ -151,19 +161,39 @@ router.get('/entries/:username', function(req, res, next) {
 
 //Here is where people take the user's quiz 
 router.get('/:username', function(req, res, next) {
-   var paramUsername = xssFilters.inHTMLData(req.params.username);
-   res.render('quiztotake', { "username": paramUsername, "error": ''}); 
+    var paramUsername = xssFilters.inHTMLData(req.params.username);
+    Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, foundquiz) {
+        if (err) {
+            res.send('Illegal request. Reported.', 400);
+        }
+        if (foundquiz) {
+            res.render('quiztotake', { "username": paramUsername, "error": '', "message": foundquiz.message });
+        }
+        else {
+            res.redirect('/quizmaker/' + paramUsername);
+        }
+    })
 });
 
 //Here is when a user makes a mistake filling out the form
 router.get('/:username/error', function(req, res, next) {
-   var paramUsername = xssFilters.inHTMLData(req.params.username);
-   res.render('quiztotake', { "username": paramUsername, "error": 'Hey! You might want to fill out all inputs!'}); 
+    var paramUsername = xssFilters.inHTMLData(req.params.username);
+    Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, foundquiz) {
+        if (err) {
+            res.send('Illegal request. Reported.', 400);
+        }
+        if (foundquiz) {
+            res.render('quiztotake', { "username": paramUsername, "error": 'Hey! You might want to fill out all inputs!', "message": foundquiz.message });
+        }
+        else {
+            res.redirect('/quizmaker/' + paramUsername);
+        }
+    })
 });
 
 
 router.post('/:username/results', function(req, res, next) {
-   var paramUsername = xssFilters.inHTMLData(req.params.username);
+    var paramUsername = xssFilters.inHTMLData(req.params.username);
     var pendingQuizAnswer =
         new Quiz({
             username: paramUsername,
@@ -188,23 +218,25 @@ router.post('/:username/results', function(req, res, next) {
         isEmpty(pendingQuizAnswer.snack) ||
         isEmpty(pendingQuizAnswer.enemy) ||
         isEmpty(pendingQuizAnswer.favsong) ||
-        isEmpty(pendingQuizAnswer.sadgong)) {
+        isEmpty(pendingQuizAnswer.sadsong)) {
         res.redirect('/' + paramUsername + '/error');
-    } else {
+    }
+    else {
         pendingQuizAnswer.save(function(err, quizAnswer) {
             if (err) {
                 res.send('Illegal request. Reported.', 400);
             }
             else {
-                Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, foundKey){
+                Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, foundKey) {
                     if (err) {
                         res.send('Illegal request. Reported.', 400);
                     }
                     if (foundKey == null) {
-                        res.render('simpleerror', { "error": "No user exists under that name", "desc": ''})
-                    } else {
-                        res.render('results', { 
-                            "username": paramUsername, 
+                        res.render('simpleerror', { "error": "No user exists under that name", "desc": '' })
+                    }
+                    else {
+                        res.render('results', {
+                            "username": paramUsername,
                             "results": comparingResults(foundKey, quizAnswer)
                         });
                     }
@@ -217,31 +249,32 @@ router.post('/:username/results', function(req, res, next) {
 //Send form to edit
 router.get('/quizeditor/:username', function(req, res, next) {
     var paramUsername = xssFilters.inHTMLData(req.params.username);
-    Quiz.count({ "username": paramUsername, "isKey": true}, function(err, numOfKeys) {
-        if(err) { 
+    Quiz.count({ "username": paramUsername, "isKey": true }, function(err, numOfKeys) {
+        if (err) {
             res.send('Illegal Request', 400);
         }
         if (numOfKeys != 0) {
-            Quiz.find({ "username": paramUsername, "isKey": true}, function(err, foundquiz){
-                if(err) { 
+            Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, foundquiz) {
+                if (err) {
                     res.send('Illegal Request', 400);
                 }
-                res.render('quizeditor', { 
-                    "username": paramUsername, 
-                    color: foundquiz[0].color,
-                    currentage: foundquiz[0].currentage,
-                    crush: foundquiz[0].crush,
-                    movie: foundquiz[0].movie,
-                    pet: foundquiz[0].pet,
-                    snack: foundquiz[0].snack,
-                    enemy: foundquiz[0].enemy,
-                    favsong: foundquiz[0].favsong,
-                    sadsong: foundquiz[0].sadsong,
-                    message: foundquiz[0].message,
-                    error: '' 
+                res.render('quizeditor', {
+                    "username": paramUsername,
+                    color: foundquiz.color,
+                    currentage: foundquiz.currentage,
+                    crush: foundquiz.crush,
+                    movie: foundquiz.movie,
+                    pet: foundquiz.pet,
+                    snack: foundquiz.snack,
+                    enemy: foundquiz.enemy,
+                    favsong: foundquiz.favsong,
+                    sadsong: foundquiz.sadsong,
+                    message: foundquiz.message,
+                    error: ''
                 })
             });
-        } else {
+        }
+        else {
             res.redirect('/quizmaker/' + paramUsername);
         }
     });
@@ -249,21 +282,21 @@ router.get('/quizeditor/:username', function(req, res, next) {
 
 //Error checking and editing stuff
 router.post('/quizeditor/:username', function(req, res, next) {
-   var paramUsername = xssFilters.inHTMLData(req.params.username);
+    var paramUsername = xssFilters.inHTMLData(req.params.username);
     var pendingQuizEdit = {
-            username: paramUsername,
-            color: xssFilters.inHTMLData(req.body.color),
-            currentage: xssFilters.inHTMLData(req.body.currentage),
-            crush: xssFilters.inHTMLData(req.body.crush),
-            movie: xssFilters.inHTMLData(req.body.movie),
-            pet: xssFilters.inHTMLData(req.body.pet),
-            snack: xssFilters.inHTMLData(req.body.snack),
-            enemy: xssFilters.inHTMLData(req.body.enemy),
-            favsong: xssFilters.inHTMLData(req.body.favsong),
-            sadsong: xssFilters.inHTMLData(req.body.sadsong),
-            message: xssFilters.inHTMLData(req.body.message),
-            isKey: true
-        };
+        username: paramUsername,
+        color: xssFilters.inHTMLData(req.body.color),
+        currentage: xssFilters.inHTMLData(req.body.currentage),
+        crush: xssFilters.inHTMLData(req.body.crush),
+        movie: xssFilters.inHTMLData(req.body.movie),
+        pet: xssFilters.inHTMLData(req.body.pet),
+        snack: xssFilters.inHTMLData(req.body.snack),
+        enemy: xssFilters.inHTMLData(req.body.enemy),
+        favsong: xssFilters.inHTMLData(req.body.favsong),
+        sadsong: xssFilters.inHTMLData(req.body.sadsong),
+        message: xssFilters.inHTMLData(req.body.message),
+        isKey: true
+    };
     if (isEmpty(pendingQuizEdit.username) ||
         isEmpty(pendingQuizEdit.color) ||
         isEmpty(pendingQuizEdit.currentage) ||
@@ -273,29 +306,31 @@ router.post('/quizeditor/:username', function(req, res, next) {
         isEmpty(pendingQuizEdit.snack) ||
         isEmpty(pendingQuizEdit.enemy) ||
         isEmpty(pendingQuizEdit.favsong) ||
-        isEmpty(pendingQuizEdit.sadgong)) {
-        Quiz.find({ "username": paramUsername, "isKey": true}, function(err, quiz){
-            if(err) { 
+        isEmpty(pendingQuizEdit.sadsong)) {
+        Quiz.findOne({ "username": paramUsername, "isKey": true }, function(err, quiz) {
+            if (err) {
                 res.send('Illegal Request', 400);
-            } else {
-                res.render('quizeditor', { 
-                    username: paramUsername, 
-                    color: quiz[0].color,
-                    currentage: quiz[0].currentage,
-                    crush: quiz[0].crush,
-                    movie: quiz[0].movie,
-                    pet: quiz[0].pet,
-                    snack: quiz[0].snack,
-                    enemy: quiz[0].enemy,
-                    favsong: quiz[0].favsong,
-                    sadsong: quiz[0].sadsong,
-                    message: quiz[0].message,
-                    error: 'Hey! You might want to fill out all inputs!' 
+            }
+            else {
+                res.render('quizeditor', {
+                    username: paramUsername,
+                    color: quiz.color,
+                    currentage: quiz.currentage,
+                    crush: quiz.crush,
+                    movie: quiz.movie,
+                    pet: quiz.pet,
+                    snack: quiz.snack,
+                    enemy: quiz.enemy,
+                    favsong: quiz.favsong,
+                    sadsong: quiz.sadsong,
+                    message: quiz.message,
+                    error: 'Hey! You might want to fill out all inputs!'
                 })
             }
         });
-    } else {
-        Quiz.findOneAndUpdate({ "username": paramUsername, "isKey": true}, {
+    }
+    else {
+        Quiz.findOneAndUpdate({ "username": paramUsername, "isKey": true }, {
             color: xssFilters.inHTMLData(req.body.color),
             currentage: xssFilters.inHTMLData(req.body.currentage),
             crush: xssFilters.inHTMLData(req.body.crush),
@@ -306,10 +341,11 @@ router.post('/quizeditor/:username', function(req, res, next) {
             favsong: xssFilters.inHTMLData(req.body.favsong),
             sadsong: xssFilters.inHTMLData(req.body.sadsong),
             message: xssFilters.inHTMLData(req.body.message)
-        }, function(err, update){
+        }, function(err, update) {
             if (err) {
                 res.send('Illegal Request. Reported.', 400);
-            } else {
+            }
+            else {
                 res.redirect('/entries/' + paramUsername);
             }
         });
